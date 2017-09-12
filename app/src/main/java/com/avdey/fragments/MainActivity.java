3ddb8d7018f349b494c73f777a59013164e32130
avdey <avdey87@gmail.com>
2017-09-12 18:48:53 +0300

    3
    3

--- app/src/main/java/com/avdey/fragments/MainActivity.java
+++ app/src/main/java/com/avdey/fragments/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity implements Fragment1.OnSelec
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.actitvity_main);
 
     }
 
