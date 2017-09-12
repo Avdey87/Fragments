6887e8084cf30803afb4cbbaf385ee4130507eee
avdey <avdey87@gmail.com>
2017-09-12 15:37:47 +0300

    1

--- app/src/main/java/com/avdey/fragments/MainActivity.java
+++ app/src/main/java/com/avdey/fragments/MainActivity.java
@@ -1,20 +1,22 @@
 package com.avdey.fragments;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements Fragment1.OnSelectButtonListern {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_wide);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -48,5 +50,18 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onButtonSelected(int buttonIndex) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        Fragment2 fragment2 = (Fragment2) fragmentManager.findFragmentById(R.id.fragment2);
+
+
+        if (fragment2 != null) {
+            fragment2.setDescription(buttonIndex);
+
+        }
     }
 }
